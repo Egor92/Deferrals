d9f3d199a7d173be4ed924cc768099700428b5f3
Novikov Egor <novikov.ea.nnov@gmail.com>
2017-01-26 23:31:26 +0300

    Code moved from other project. One test class implemented #1

---
+++ src/Egor92.Deferrals/Deferral.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Egor92.Deferrals
+{
+    public interface IDeferral
+    {
+        bool IsCompleted { get; }
+
+        event EventHandler Completed;
+
+        void Complete();
+    }
+
+    public class Deferral : IDeferral
+    {
+        #region Fields
+
+        private readonly object _completeSyncRoot = new object();
+
+        #endregion
+
+        #region Implementation of IDeferral
+
+        public bool IsCompleted { get; private set; }
+
+        #region Completed
+
+        public event EventHandler Completed;
+
+        private void RaiseContinueInvoked()
+        {
+            Completed?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        public void Complete()
+        {
+            lock (_completeSyncRoot)
+            {
+                if (IsCompleted)
+                    return;
+                IsCompleted = true;
+                RaiseContinueInvoked();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Egor92.Deferrals/DeferralSource.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Egor92.Deferrals
+{
+    public class DeferralSource
+    {
+        #region Fields
+
+        private bool _isCompleted;
+        private readonly Action _deferrableAction;
+        private readonly IList<IDeferral> _deferrals = new List<IDeferral>();
+
+        #endregion
+
+        #region Ctor
+
+        public DeferralSource(Action deferrableAction)
+        {
+            if (deferrableAction == null)
+                throw new ArgumentNullException(nameof(deferrableAction));
+            _deferrableAction = deferrableAction;
+        }
+
+        #endregion
+
+        #region Implementation of IDisposable
+
+        //public void Dispose()
+        //{
+        //    foreach (var continuable in _deferrals)
+        //    {
+        //        continuable.Completed -= OnDeferralCompleted;
+        //    }
+        //}
+
+        #endregion
+
+        public IDeferral CreateDeferral()
+        {
+            var continuable = new Deferral();
+            _deferrals.Add(continuable);
+            continuable.Completed += OnDeferralCompleted;
+            return continuable;
+        }
+
+        public void Invoke()
+        {
+            InvokeIfCan();
+        }
+
+        private void InvokeIfCan()
+        {
+            var canContinue = _deferrals.All(x => x.IsCompleted);
+            if (!canContinue)
+                return;
+
+            _deferrableAction();
+        }
+
+        private void OnDeferralCompleted(object sender, EventArgs e)
+        {
+            InvokeIfCan();
+        }
+    }
+}
