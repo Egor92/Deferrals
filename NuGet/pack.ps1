69742e94317132535f5651920d455549ad31bb8b
Egor Novikov <novikov.ea@mail.ru>
2017-03-22 00:09:34 +0300

    Configured to publish nuget packages #3

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\Egor92.Deferrals\bin\Release\Egor92.Deferrals.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\Egor92.Deferrals.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\NuGet\Egor92.Deferrals.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\NuGet\Egor92.Deferrals.compiled.nuspec
